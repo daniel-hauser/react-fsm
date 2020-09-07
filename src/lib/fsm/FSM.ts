a45003ffbe257e1f474624ddfd158ef66a9ac3d7
Daniel <dd.hauser@gmail.com>
2020-09-07 22:54:55 +0300

    Initial FSM lib

---
+++ src/lib/fsm/FSM.ts
@@ -0,0 +1,65 @@
+export type StateName = string;
+export type ActionName = string;
+export type States = Map<StateName, Actions>;
+export type Actions = Map<ActionName, StateName> | null;
+
+export class FSM {
+  #states: States;
+  #currentState: StateName;
+
+  constructor(states: States, initial?: StateName) {
+    if (states.size === 0) {
+      throw new Error(`states must not be empty`);
+    }
+
+    if (initial !== undefined) {
+      if (!states.has(initial)) {
+        throw new Error(`Initial state "${initial}" is not a valid state`);
+      }
+
+      this.#currentState = initial;
+    } else {
+      // Use first inserted key as initial
+      // Note from MDN: The keys() method returns the keys in insertion order.
+      this.#currentState = states.keys().next().value;
+    }
+
+    this.#states = states;
+  }
+
+  get currentState() {
+    return this.#currentState;
+  }
+
+  get allowedActions(): Set<ActionName> {
+    return new Set(this.#states.get(this.#currentState)?.keys());
+  }
+
+  isValidAction(action: ActionName) {
+    return this.allowedActions.has(action);
+  }
+
+  assertValidAction(action?: ActionName | null): asserts action {
+    if (!action || !this.isValidAction(action)) {
+      throw new Error(`State "${this.currentState}" has no "${action}" action`);
+    }
+  }
+
+  assertValidState(state?: StateName | null): asserts state {
+    if (!state || !this.#states.has(state)) {
+      throw new Error(`State "${state}" is not valid`);
+    }
+  }
+
+  doAction(action: ActionName) {
+    this.assertValidAction(action);
+
+    // I use `!` because we should allays have a state entry for
+    //  the current state as its asserted in assertValidAction
+    const nextState = this.#states.get(this.#currentState)!.get(action);
+
+    this.assertValidState(nextState);
+
+    this.#currentState = nextState;
+  }
+}
---
+++ src/lib/fsm/__tests__/FSM.test.ts
@@ -0,0 +1,88 @@
+import { FSM } from "../FSM";
+
+describe("Initial state", () => {
+  test("Must be a valid state", () => {
+    expect(() => new FSM(new Map([["state a", null]]), "state b")).toThrow(
+      /is not a valid state/
+    );
+    expect(
+      () => new FSM(new Map([["state a", null]]), "state a")
+    ).not.toThrow();
+  });
+
+  test("Is set as currentState", () => {
+    const fsm = new FSM(new Map([["state a", null]]), "state a");
+
+    expect(fsm.currentState).toBe("state a");
+  });
+
+  test("Picks the first state if no initial is sent", () => {
+    const fsm = new FSM(
+      new Map([
+        ["state a", null],
+        ["state b", null],
+      ])
+    );
+    expect(fsm.currentState).toBe("state a");
+  });
+});
+
+describe("Lifecycle", () => {
+  test("States must exist", () => {
+    expect(() => new FSM(new Map())).toThrow(/states must not be empty/);
+  });
+
+  test("Action must exist", () => {
+    const fsm = new FSM(
+      new Map([
+        ["state a", new Map([["goto b", "state b"]])],
+        ["state b", null],
+      ]),
+      "state a"
+    );
+    expect(() => fsm.doAction("goto a")).toThrow();
+
+    expect(() => fsm.doAction("goto b")).not.toThrow();
+    expect(fsm.currentState).toBe("state b");
+
+    expect(() => fsm.doAction("goto a")).toThrow();
+  });
+
+  test("Target state must exist", () => {
+    const fsm = new FSM(
+      new Map([["state a", new Map([["goto b", "state b"]])]]),
+      "state a"
+    );
+
+    expect(() => fsm.doAction("goto b")).toThrow();
+  });
+
+  test("State is updated correctly", () => {
+    const fsm = new FSM(
+      new Map([
+        ["state a", new Map([["goto b", "state b"]])],
+        ["state b", null],
+      ]),
+      "state a"
+    );
+    fsm.doAction("goto b");
+
+    expect(fsm.currentState).toBe("state b");
+  });
+});
+
+describe("Allowed actions", () => {
+  test("Are correct", () => {
+    const fsm = new FSM(
+      new Map([
+        ["state a", new Map([["goto b", "state b"]])],
+        ["state b", null],
+      ]),
+      "state a"
+    );
+
+    expect(fsm.allowedActions).toEqual(new Set(["goto b"]));
+    fsm.doAction("goto b");
+    expect(fsm.allowedActions).toEqual(new Set());
+  });
+});
